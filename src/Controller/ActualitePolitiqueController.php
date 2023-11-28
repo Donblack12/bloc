d47312bea4bffb36392239d291aa043862af419b
pau-01 <donblackimboyo@gmail.com>
2023-11-29 00:29:20 +0100

    donbeni

--- src/Controller/ActualitePolitiqueController.php
+++ src/Controller/ActualitePolitiqueController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\CommentFormType;
+use App\Repository\CommentRepository; // Assurez-vous d'importer le CommentRepository
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,12 +21,27 @@ class ActualitePolitiqueController extends AbstractController
     /**
      * @Route("/actualite-politique", name="actualite_politique")
      */
-    public function index(): Response
+    public function index(Request $request, CommentRepository $commentRepository): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('actualite_politique');
+        }
+
+        // Utilisation de l'injection de dépendance pour le repository
+        $comments = $commentRepository->findAll();
+
         return $this->render('actualite_politique/actualite_politique.html.twig', [
             'actualites' => $this->actualites,
+            'commentForm' => $form->createView(), // Assurez-vous que cette clé correspond à celle utilisée dans le template Twig
+            'comments' => $comments,
         ]);
     }
 }
-
-
