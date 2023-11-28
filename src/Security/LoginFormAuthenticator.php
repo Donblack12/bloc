e73917761e5ad7287a857299ac7d4242670aecb9
pau-01 <donblackimboyo@gmail.com>
2023-11-28 15:33:58 +0100

    donbeni

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -1,7 +1,8 @@
 <?php
 namespace App\Security;
 
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use App\Entity\User; // Assurez-vous que c'est le bon chemin vers votre entité User
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
@@ -12,51 +13,71 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 
 class LoginFormAuthenticator implements AuthenticatorInterface
 {
-private RouterInterface $router;
+    private RouterInterface $router;
+    private EntityManagerInterface $entityManager;
 
-public function __construct(RouterInterface $router)
-{
-$this->router = $router;
-}
+    public function __construct(RouterInterface $router, EntityManagerInterface $entityManager)
+    {
+        $this->router = $router;
+        $this->entityManager = $entityManager;
+    }
 
-public function supports(Request $request): ?bool
-{
-// Activez cet authenticator pour une route spécifique ou une condition
-return $request->attributes->get('_route') === 'app_register';
-}
+    public function supports(Request $request): ?bool
+    {
+        // Activez cet authenticator pour les routes de connexion et d'inscription
+        return in_array($request->attributes->get('_route'), ['app_login', 'app_register']);
+    }
 
-public function authenticate(Request $request): Passport
-{
-// Récupération de l'utilisateur de la session
-$user = $request->getSession()->get('user');
-if (!$user instanceof UserInterface) {
-throw new AuthenticationException('No user found in session.');
-}
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
 
-// Création d'un UserBadge avec l'identifiant de l'utilisateur
-$userBadge = new UserBadge($user->getUserIdentifier());
+        if (!$email || !$password) {
+            throw new CustomUserMessageAuthenticationException('Email et mot de passe sont requis.');
+        }
 
-return new SelfValidatingPassport($userBadge);
-}
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
 
-public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
-{
-// Redirection après une authentification réussie
-return new Response($this->router->generate('actualite_politique'));
-}
+        if (!$user) {
+            throw new UserNotFoundException('Aucun utilisateur trouvé avec cet email.');
+        }
 
-public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
-{
-// Gérer l'échec de l'authentification si nécessaire
-return new Response($exception->getMessageKey(), Response::HTTP_FORBIDDEN);
-}
+        // Ici, vous pouvez ajouter la logique pour vérifier le mot de passe
+        // Si le mot de passe ne correspond pas, lancez une exception
 
-public function createToken(Passport $passport, string $firewallName): TokenInterface
-{
-// Création et retour d'un TokenInterface
-return new UsernamePasswordToken($passport->getUser(), $firewallName, $passport->getUser()->getRoles());
-}
+        $userBadge = new UserBadge($user->getUserIdentifier());
+
+        return new SelfValidatingPassport($userBadge);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        if ($request->attributes->get('_route') === 'app_login') {
+            return new Response($this->router->generate('actualite_politique'));
+        }
+
+        // Redirection après l'inscription si nécessaire
+        // Par exemple, retourner à la page d'accueil ou à une page de bienvenue
+
+        return new Response($this->router->generate('une_autre_route'));
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        // Gérer l'échec de l'authentification si nécessaire
+        // Vous pouvez rediriger vers la page de connexion avec un message d'erreur
+        return new Response($exception->getMessageKey(), Response::HTTP_FORBIDDEN);
+    }
+
+    public function createToken(Passport $passport, string $firewallName): TokenInterface
+    {
+        return new UsernamePasswordToken($passport->getUser(), $firewallName, $passport->getUser()->getRoles());
+    }
 }
